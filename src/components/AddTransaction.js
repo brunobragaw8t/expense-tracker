1dab95b7c9d4f228cc6a4d737dd1662b771f7b3a
Bruno Braga <brunobragaw8t@gmail.com>
2021-12-27 16:58:41 +0000

    Fix HTML class attribute for JSX

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -10,23 +10,23 @@ export const AddTransaction = () => {
         <h4 className="border-bottom pb-2 mb-3">Add a new transaction</h4>
 
         <form>
-          <div class="mb-3">
-            <label htmlFor="title" class="form-label">Title</label>
+          <div className="mb-3">
+            <label htmlFor="title" className="form-label">Title</label>
 
-            <input type="text" class="form-control" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+            <input type="text" className="form-control" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
           </div>
 
-          <div class="mb-3">
-            <label htmlFor="amount" class="form-label">Amount</label>
+          <div className="mb-3">
+            <label htmlFor="amount" className="form-label">Amount</label>
 
             <div className="input-group">
-              <input type="number" step="0.01" class="form-control" id="amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
+              <input type="number" step="0.01" className="form-control" id="amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
 
               <span className="input-group-text">€</span>
             </div>
           </div>
 
-          <button type="submit" class="btn btn-primary w-100">Submit</button>
+          <button type="submit" className="btn btn-primary w-100">Submit</button>
         </form>
       </div>
     </div>
